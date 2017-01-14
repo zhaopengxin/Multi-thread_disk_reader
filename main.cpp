13d52450542ef18386b9952b77de4f36b2f4708e
Zhao Pengxin <zhaopengxin@0587379993.wireless.umich.net>
2017-01-13 22:41:20 -0500

    some test

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ void servicer(void *a){
 void open(void *a[]){
 	char **argv = (char **) a;
 	max_disk_queue = atoi(argv[1]);
-	//thread servicer ((thread_startfunc_t) service, (void *) argv[1]);
+	thread servicer ((thread_startfunc_t) service, (void *) argv[1]);
 	for (int i = 2; i < sizeof(argv)/sizeof(argv[1]); ++i)
 	{
 		//char thread_name[] = {'t', (char) i};
